9da65357f1c199d19ce21a1091e787ecd7a6013b
Mandhouj A <nouitou@yahoo.fr>
2021-10-16 04:07:16 +0200

    fix erreurs dropdown

--- docs/js/dropdown.js
+++ docs/js/dropdown.js
@@ -12,7 +12,7 @@ let textInputUstens     = document.querySelector('.ustensil')
 let item                = document.querySelectorAll('.item')
 let spanIngredient      = document.querySelector('.ingredient')
 let spanAppliance       = document.querySelector('.appliance')
-let spanUstensils       = document.querySelector('.ustensils')
+let spanUstensils       = document.querySelector('.ustensil')
 let chevronIngredient   = document.querySelector('.chevron-ingredient')
 let chevronAppliance    = document.querySelector('.chevron-appliance')
 let chevronUstensil     = document.querySelector('.chevron-ustensil')
@@ -22,8 +22,9 @@ const inputAppliance    =  document.querySelector('.button--green');
 const inputUsetensils   = document.querySelector('.button--red');
 const mainInput         = document.querySelector('.form-control');
 const menuD             = document.querySelector('.menu-d')
-let dropdownBody        = document.querySelectorAll('.dropdown-body')
-let dropdownHead        = document.querySelectorAll('.dropdown-head')
+const btnAppliance        = document.querySelector('.btn-appliance')
+const btnUstensil        = document.querySelector('.btn-ustensils')
+const btnIngredient        = document.querySelector('.btn-ingredient')
 const bodyIngr          = document.getElementById('dropdown-body-Ingredients')
 const bodyApp          = document.getElementById('dropdown-body-Appareil')
 const bodyUstens         = document.getElementById('dropdown-body-Ustensiles')
@@ -34,13 +35,16 @@ showIngredients();
 
 /**display the drop-down */
 chevronIngredient.addEventListener('click', () => {
-   bodyIngr.classList.toggle('active')
+   bodyIngr.classList.toggle('active');
+   btnIngredient.style.width ="29vw"
 })
 chevronAppliance.addEventListener('click', () => {
    bodyApp.classList.toggle('active')
+   btnAppliance.style.width ="29vw"
 })
 chevronUstensil.addEventListener('click', () => {
    bodyUstens.classList.toggle('active')
+   btnUstensil.style.width ="29vw"
 })
 
 /**THE span when input is written */
@@ -54,12 +58,8 @@ mainInput.addEventListener('input', ()=>{
         textInput.style.visibility = "visible" 
 })
 
-// /**on click on th items */  
 
-// item.forEach(elt => {     
-// elt.addEventListener('click', () =>{
-//     inputResult.innerHTML = elt.options[elt.selectedIndex].value;
-//             textInput.style.visibility = "visible" })})
+
 /*in ingredient*/
 inputIngredient.addEventListener('input', ()=>{
         if(inputIngredient.value.length>2){
@@ -75,7 +75,7 @@ inputAppliance.addEventListener('input', ()=>{
     if(inputUsetensils.value.length>2){
         //activate the span
         spanUstensils.innerHTML = inputUsetensils.value;
-        textInput.style.visibility = "visible";}})
+        textInputUstens.style.visibility = "visible";}})
      
 
 
@@ -86,7 +86,7 @@ function showAppliances(){
     }
     let noDoubleAppliance = applianceItems.filter((item, index)=>applianceItems.indexOf(item)===index).sort()
     for (let k=0; k<noDoubleAppliance.length; k++){
-        appliances.innerHTML += `<li class="item" value="${noDoubleAppliance[k]}" >${noDoubleAppliance[k]}</li>`;
+        appliances.innerHTML += `<li class="item" data-value="${noDoubleAppliance[k]}" >${noDoubleAppliance[k]}</li>`;
  
 
 
@@ -99,7 +99,7 @@ function showUstensils() {
     ustensilsItem.push(recipes[i].ustensils[0].toLocaleLowerCase()) }
     let noDoubleUstensils = ustensilsItem.filter((item, index)=>ustensilsItem.indexOf(item)===index).sort()
     for (let k=0; k<noDoubleUstensils.length; k++){   
-    ustensils.innerHTML += `<li class="item" value='${noDoubleUstensils[k]}'>${noDoubleUstensils[k]}</li>`;
+    ustensils.innerHTML += `<li class="item" data-value='${noDoubleUstensils[k]}'>${noDoubleUstensils[k]}</li>`;
 }}
 
 function showIngredients() {
@@ -114,7 +114,7 @@ function showIngredients() {
           // console.log(ingredientItem)
         let noDoubleIngredient = ingredientItem.filter((item, index)=>ingredientItem.indexOf(item)===index).sort();
         for (let i = 0; i < noDoubleIngredient.length; i++) {       
-             Ingredients.innerHTML += `<li class="item" value='${noDoubleIngredient[i]}'>${noDoubleIngredient[i]}</li>`; 
+             Ingredients.innerHTML += `<li data-class="item" value='${noDoubleIngredient[i]}'>${noDoubleIngredient[i]}</li>`; 
         }
     
     }
